920ee75665d2efbf2a02a8c118673027c21cec41
Mark Olbert <mark@arcabama.com>
2017-05-15 14:38:30 -0700

    save changes prior to branching to test eliminating AutoFac from WixUI

---
+++ LanHistorySetupApp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using Olbert.Wix;
+using Olbert.Wix.ViewModels;
+
+namespace Olbert.LanHistorySetupUI
+{
+    public class LanHistorySetupApp : WixApp
+    {
+        protected override void OnAction( EngineActionEventArgs args )
+        {
+            base.OnAction( args );
+            if( args == null ) return;
+
+            switch( args.Action )
+            {
+                case LaunchAction.Install:
+                    if( WixViewModel.InstallState == InstallState.NotPresent )
+                    {
+                        args.Processed = true;
+
+                        Engine.Plan( LaunchAction.Install );
+                    }
+                    else args.Message = "The software is already installed";
+
+                    break;
+
+                case LaunchAction.Uninstall:
+                    if( WixViewModel.InstallState == InstallState.Present )
+                    {
+                        args.Processed = true;
+
+                        Engine.Plan( LaunchAction.Uninstall );
+                    }
+                    else args.Message = "The software is not installed";
+
+                    break;
+
+                default:
+                    args.Processed = false;
+                    args.Message = $"{args.Action} not implemented by {nameof(LanHistorySetupApp)}";
+
+                    break;
+            }
+        }
+    }
+}
---
+++ LanHistorySetupViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Olbert.Wix;
+using Olbert.Wix.buttons;
+using Olbert.Wix.panels;
+using Olbert.Wix.ViewModels;
+
+namespace Olbert.LanHistorySetupUI
+{
+    public class LanHistorySetupViewModelAutofac : WixAutofacModule<LanHistorySetupViewModel>
+    {
+    }
+
+    public class LanHistorySetupViewModel : WixViewModel
+    {
+        private enum Stage
+        {
+            Intro,
+            License,
+            Finish
+        }
+
+        private Stage _stage = Stage.Intro;
+        private readonly string _license;
+        private readonly string _intro;
+
+        public LanHistorySetupViewModel()
+        {
+            WindowTitle = "LanHistory Installer";
+
+            _license = GetEmbeddedTextFile( "license.rtf" );
+            _intro = GetEmbeddedTextFile( "intro.rtf" );
+
+            var vm = new TextPanelViewModel { Text = _intro };
+
+            CurrentButtons = new WixStandardButtons() { DataContext = vm.GetButtonsViewModel() };
+            CurrentPanel = new WixTextScroller { DataContext = vm };
+        }
+
+        protected override void MoveNext()
+        {
+            switch( _stage )
+            {
+                case Stage.Intro:
+                    var introVM = new LicensePanelViewModel { Text = _license };
+
+                    CurrentButtons.DataContext = introVM.GetButtonsViewModel();
+                    CurrentPanel = new WixLicense { DataContext = introVM };
+
+                    _stage = Stage.License;
+
+                    break;
+
+                case Stage.License:
+                    var finishVM = new FinishPanelViewModel();
+
+                    CurrentButtons.DataContext = finishVM.GetButtonsViewModel();
+                    CurrentPanel = new WixLicense { DataContext = finishVM };
+
+                    _stage = Stage.Finish;
+
+                    break;
+            }
+        }
+
+        protected override void MovePrevious()
+        {
+            switch( _stage )
+            {
+                case Stage.License:
+                    break;
+
+                case Stage.Finish:
+                    break;
+            }
+        }
+
+    }
+}
